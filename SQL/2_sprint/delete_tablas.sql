f8f78d64eaa63f7dc423119c11e7afd7239c848e
splinterzealotDEV <minimi_ta@hotmail.com>
2017-12-01 18:22:38 -0600

    Add files via upload

--- SQL/2_sprint/delete_tablas.sql
+++ SQL/2_sprint/delete_tablas.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS `UEFA`.`del_UEFA` ;
 CREATE TABLE IF NOT EXISTS `UEFA`.`del_UEFA` (
   `id` INT NOT NULL,
   `id_record` INT NOT NULL,
+  `id_db` INT NOT NULL,
   `id_tabla` INT NOT NULL,
   
   PRIMARY KEY (`id`))
@@ -13,6 +14,7 @@ DROP TABLE IF EXISTS `Liga`.`del_Liga` ;
 CREATE TABLE IF NOT EXISTS `Liga`.`del_Liga` (
   `id` INT NOT NULL,
   `id_record` INT NOT NULL,
+  `id_db` INT NOT NULL,
   `id_tabla` INT NOT NULL,
   
   PRIMARY KEY (`id`))
@@ -21,6 +23,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `UEFA`.`del_Liga_F` (
   `id` INT NOT NULL,
   `id_record` INT NOT NULL,
+  `id_db` INT NOT NULL,
   `id_tabla` INT NOT NULL,
   
   PRIMARY KEY (`id`))
@@ -29,6 +32,7 @@ ENGINE=FEDERATED DEFAULT CHARACTER SET = utf8 CONNECTION='mysql://user1:user1@10
 CREATE TABLE IF NOT EXISTS `Liga`.`del_UEFA_F` (
   `id` INT NOT NULL,
   `id_record` INT NOT NULL,
+   `id_db` INT NOT NULL,
   `id_tabla` INT NOT NULL,
   
   PRIMARY KEY (`id`))
